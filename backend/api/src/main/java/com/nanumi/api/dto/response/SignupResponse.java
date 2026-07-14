b3bec25ea98469c9fd1335a5c2973d6511fc0c62
kch-chan <kch.c1216@gmail.com>
2026-07-14 13:30:25 +0900

    feat: signup response 작성 (#60)

---
+++ backend/api/src/main/java/com/nanumi/api/dto/response/SignupResponse.java
@@ -0,0 +1,13 @@
+package com.nanumi.api.dto.response;
+
+public record SignupResponse(
+  String message,
+  UserResponse user
+) {
+  public static SignupResponse of (UserResponse user) {
+    return new SignupResponse(
+      "회원가입에 성공하였습니다",
+      user
+    );
+  }
+}
