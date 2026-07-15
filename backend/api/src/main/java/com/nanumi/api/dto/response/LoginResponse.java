64d42df05eea7904e08b784475adf4113b3e3b47
kch-chan <kch.c1216@gmail.com>
2026-07-15 10:09:59 +0900

    feat: login response 작성 (#61)

---
+++ backend/api/src/main/java/com/nanumi/api/dto/response/LoginResponse.java
@@ -0,0 +1,15 @@
+package com.nanumi.api.dto.response;
+
+public record LoginResponse(
+  String accessToken,
+  String refreshToken,
+  UserResponse user
+) {
+  public static LoginResponse of (
+    String accessToken,
+    String refreshToken,
+    UserResponse user
+  ) {
+    return new LoginResponse(accessToken, refreshToken, user);
+  }
+}
